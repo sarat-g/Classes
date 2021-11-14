db1a082aa1f9520aa869bae6c9708d5b271487f5
sarat-g <sarat.gudey@gmail.com>
2021-11-14 03:01:50 -0500

    Main class created to check the Person class and the constructors

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        Person p1 = new Person();
+        Person p2 = new Person("ASDF", 30, 'M');
+        System.out.println(p2.getGender());
+    }
+
+}
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -2,4 +2,40 @@ public class Person {
     String Name;
     int Age;
     char Gender;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+
+    public char getGender() {
+        System.out.println("Gender should be printed after this");
+        return Gender;
+    }
+
+    public void setGender(char gender) {
+        Gender = gender;
+    }
+
+    public Person() {
+        System.out.println("No parameters for this constructor");
+    }
+
+    public Person(String name, int age, char gender) {
+        System.out.println("this is a parametric constructor");
+        Name = name;
+        Age = age;
+        Gender = gender;
+    }
 }
